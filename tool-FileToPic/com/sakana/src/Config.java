afe562e018b5ed4fdd782d2ab42903069f6a631e
sakana-7z <twz555800955@163.com>
2024-10-11 01:59:33 +0800

    Add files via upload

---
+++ tool-FileToPic/com/sakana/src/Config.java
@@ -0,0 +1,13 @@
+package com.sakana.src;
+
+import java.io.BufferedReader;
+import java.io.File;
+
+import java.io.IOException;
+
+public class Config {
+    public static String DIRECTORY_PATH;
+    //="E:\\JAVA\\练习文件夹\\实验"
+
+}
+
